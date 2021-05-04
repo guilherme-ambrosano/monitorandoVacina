35a3202d6e72a950bee9f82fde669663f9a8407f
Guilherme Ambrosano <guibovi2@gmail.com>
2021-05-04 12:19:12 -0300

    Arrumei os erros e coloquei o som de alerta na pasta

--- monitorandoVacinapy.py
+++ monitorandoVacinapy.py
@@ -16,11 +16,20 @@ link_antigo = "https://web.archive.org/web/20210305165929/https://vacinapira.pir
 link_antigo2 = "https://web.archive.org/web/20210501110705/https://vacinapira.piracicaba.sp.gov.br/cadastro/blk_inicial/"
 
 def monitorar_vacinas(link, str_busca):
-    resp = req.get(link).text
-    soup = BeautifulSoup(resp, features="html5lib")
-    row = soup.find_all("div", {"class": "row"})[0]
 
-    classes = row.find_all("fieldset", {"class": "documentos"})
+    try:
+        resp = req.get(link).text
+    except:
+        print("Falhou")
+
+    soup = BeautifulSoup(resp, features="html5lib")
+    row = soup.find_all("div", {"class": "row"})
+
+    if len(row) == 0:
+        print("Falhou")
+        return
+
+    classes = row[0].find_all("fieldset", {"class": "documentos"})
     classes = list(map(lambda x: x.find_all("p")[1].text, classes))
 
     if len(classes) == 0:
@@ -38,9 +47,6 @@ while True:
     if agora >= datetime_busca:
         print()
         print("Acessando o site...")
-        try:
-            monitorar_vacinas(link_atual, busca)
-            time.sleep(30)
-        except:
-            print("Falhou")
+        monitorar_vacinas(link_atual, busca)
+        time.sleep(30)
 
