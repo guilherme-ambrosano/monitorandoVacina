b6dea076a2439cae43a3ac478eab76a849a0a27f
Guilherme Ambrosano <guibovi2@gmail.com>
2021-06-11 16:29:18 -0300

    arrumei o .gitignore

--- configuracoes.py
+++
@@ -1,5 +0,0 @@
-# Configurações pro monitoramento do site da vacina
-
-# lista de strings para buscar no site (por exemplo, 60 anos)
-busca = ["56", "57"] 
-horario = "12:00"  # hora que vai abrir (formato %H:%M)
